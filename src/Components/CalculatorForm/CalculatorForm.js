1037dca1db509a98a390817d35a5e77c8087e7ed
tian <tian_chaiya12@hotmail.com>
2017-11-28 20:49:56 +0700

    create basic form interface, next up is to create control component and basic client-side validation

--- src/Components/CalculatorForm/CalculatorForm.js
+++ src/Components/CalculatorForm/CalculatorForm.js
@@ -2,10 +2,50 @@ import React, { Component } from 'react';
 import './CalculatorForm.css';
 
 class CalculatorForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      coupons: []
+    };
+  }
+
   render() {
+    const appliedCoupons = this.state.coupons.map(coupon => {
+      return (
+        <p className="coupon">
+          <span>{coupon}</span>
+        </p>
+      )
+    })
+
     return (
       <div className="CalculatorForm">
-        <h1>Calculator Form</h1>
+        <form>
+          <h1>Calculator Form</h1>
+          <fieldset>
+            <legend>Bill Calculator</legend>
+            <p>
+              <label htmlFor="people">
+                <span>Number of People</span>
+                <strong>*</strong>
+              </label>
+              <input type="text" name="people" id="size_1"/>
+            </p>
+            <section>
+              {appliedCoupons}
+              <p>
+                <label htmlFor="coupon">
+                  <span>Coupon: </span>
+                </label>
+                <input type="text" id="coupon-input" name="coupon"/>
+                <button>Add</button>
+              </p>
+            </section>
+            <p>
+              <button type="submit">Submit</button>
+            </p>
+          </fieldset>
+        </form>
       </div>
     );
   }
