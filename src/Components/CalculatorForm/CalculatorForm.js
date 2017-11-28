9635108294e16f83c77f68025189e11bac94edfc
tian <tian_chaiya12@hotmail.com>
2017-11-28 23:26:02 +0700

    make CalculatorForm a controlled component with some validation logic, next up is writing onSubmit logic that dispatches a redux action, and then write tests for the component

--- src/Components/CalculatorForm/CalculatorForm.js
+++ src/Components/CalculatorForm/CalculatorForm.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
+import shortid from 'shortid'
 import './CalculatorForm.css';
 
 class CalculatorForm extends Component {
   constructor() {
     super();
     this.state = {
+      people: 1,
+      couponInput: '',
       coupons: []
     };
+    this.handlePeopleChange = this.handlePeopleChange.bind(this)
+    this.handleCouponChange = this.handleCouponChange.bind(this)
+    this.addCoupon = this.addCoupon.bind(this)
+  }
+
+  handlePeopleChange(e) { this.setState({people: e.target.value}) }
+
+  handleCouponChange(e) { this.setState({couponInput: e.target.value}) }
+
+  addCoupon(e) {
+    e.preventDefault();
+    this.setState((prevState) => {
+      const newCoupon = prevState.couponInput.toUpperCase().trim().replace(/\s\s+/g, ' ')
+      return (
+        !prevState.coupons.includes(newCoupon) && {
+          couponInput: '',
+          coupons: [...prevState.coupons, newCoupon]
+        }
+      )
+    })
+  }
+
+  deleteCoupon(coupon) {
+    this.setState((prevState) => {
+      // no need for id here, as coupon body cannot be identical.
+      return {coupons: prevState.coupons.filter(c => c !== coupon)}
+    })
   }
 
   render() {
     const appliedCoupons = this.state.coupons.map(coupon => {
       return (
-        <p className="coupon">
+        <p key={shortid.generate()} className="coupon">
           <span>{coupon}</span>
+          <button onClick={this.deleteCoupon.bind(this, coupon)}>Delete</button>
         </p>
       )
     })
@@ -26,19 +57,36 @@ class CalculatorForm extends Component {
             <legend>Bill Calculator</legend>
             <p>
               <label htmlFor="people">
-                <span>Number of People</span>
+                <span>Number of People: </span>
                 <strong>*</strong>
               </label>
-              <input type="text" name="people" id="size_1"/>
+              <input
+                type="number"
+                name="people"
+                id="people-input"
+                onChange={this.handlePeopleChange}
+                value={this.state.people}
+                required
+                min="1"
+                max="60"
+              />
             </p>
-            <section>
+            <section className="coupon-section">
               {appliedCoupons}
-              <p>
+              <p className="coupon-form">
                 <label htmlFor="coupon">
                   <span>Coupon: </span>
                 </label>
-                <input type="text" id="coupon-input" name="coupon"/>
-                <button>Add</button>
+                <input
+                  type="text"
+                  id="coupon-input"
+                  name="coupon"
+                  onChange={this.handleCouponChange}
+                  value={this.state.couponInput}
+                  minLength="1"
+                  maxLength="100"
+                />
+                <button onClick={this.addCoupon}>Add</button>
               </p>
             </section>
             <p>
