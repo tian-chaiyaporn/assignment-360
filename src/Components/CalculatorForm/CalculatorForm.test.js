4aaf773e5f3f9c2029fe28fe3d3243254fcb3dc3
tian <tian_chaiya12@hotmail.com>
2017-11-29 11:39:49 +0700

    add basic unit test for component: handle people input change, handle coupon input change, handle adding coupon, handle deleting coupon

--- src/Components/CalculatorForm/CalculatorForm.test.js
+++ src/Components/CalculatorForm/CalculatorForm.test.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { CalculatorFormTest } from './CalculatorForm';
 
-
 describe('component renders correctly', () => {
-  it('renders without crashing', () => {
-    shallow(<CalculatorFormTest />);
-  });
+  const wrapper = shallow(<CalculatorFormTest />);
 
-  // it('renders textinput and number correctly', () => {
-  //   const wrapper = shallow(<CalculatorFormTest />);
-  //   wrapper.find('people-input').simulate('change', {target: {value: 3}});
-  //   expect(wrapper.find('people-input').value)
-  // })
+  it('renders without crashing', () => { wrapper });
+
+  it('renders number correctly', () => {
+    wrapper.find('#people-input').simulate('change', {target: {value: 3}});
+    expect(wrapper.find('#people-input').props().value).toBe(3)
+  })
+
+  it('renders coupon input correctly', () => {
+    wrapper.find('#coupon-input').simulate('change', {target: {value: 'testing'}});
+    expect(wrapper.find('#coupon-input').props().value).toBe('testing')
+  })
+
+  it('renders added coupons correctly', () => {
+    const testValue = 'testing'
+    wrapper.find('#coupon-input').simulate('change', {target: {value: testValue}})
+    wrapper.find('#coupon-add').simulate('click', {preventDefault: () => {}})
+    expect(wrapper.find(`#${testValue.toUpperCase()}`).props().children).toBe(testValue.toUpperCase())
+  })
+
+  it('renders deleted coupons correctly', () => {
+    const testValue = 'testing'
+    wrapper.find('#coupon-input').simulate('change', {target: {value: testValue}})
+    wrapper.find('#coupon-add').simulate('click', {preventDefault: () => {}})
+    wrapper.find(`#${testValue.toUpperCase()} + button`).simulate('click', {preventDefault: () => {}})
+    expect(wrapper.find(`#${testValue.toUpperCase()}`).length).toBe(0)
+  })
 })
