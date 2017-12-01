d6568dedbdb0c7aa2cacfe3d90429fb9ce850764
tian <tian_chaiya12@hotmail.com>
2017-12-01 09:37:19 +0700

    REFACTOR initial state and filterDeals function to make it cleaner

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -5,56 +5,35 @@ const initialState = {
     {
       deal: 'Discount 15% for coupon code "LUCKY ONE" or the bill is more than 1000 Baht',
       discountPercent: 15,
-      conditions: {
-        type: 'OR',
-        coupon: 'LUCKY ONE',
-        pay: '>1000'
-      }
+      conditions: '_coupon === "LUCKY ONE" || _pay > 1000'
     },
     {
       deal: 'Come 4 pay 3 when they present coupon code as "4PAY3"',
       discountPercent: 25,
-      conditions: {
-        type: 'AND',
-        coupon: '4PAY3',
-        people: '===4'
-      }
+      conditions: '_coupon === "4PAY3" && _people >= 4'
     },
     {
       deal: 'Discount 20% for 2 customer when they present a coupon code as "LUCKY TWO"',
       discountPercent: 20,
-      conditions: {
-        type: 'AND',
-        coupon: 'LUCKY TWO',
-        people: '===2'
-      }
+      conditions: '_coupon === "LUCKY TWO" && _people >= 2'
     },
     {
       deal: 'Discount 25% when the bill is over 6000 Baht but it exclude all promotion',
       discountPercent: 25,
-      conditions: {
-        type: 'AND',
-        pay: '>6000'
-      }
+      conditions: '_pay > 6000' // these strings can be built by admin with the right UI
     }
   ]
 }
 
 // this should cover most current and future deals
 function filterDeals(deal, coupons, totalPeople, totalBill) {
-  const conditions = deal.conditions;
-  const couponOk   = conditions.coupon ? coupons.includes(conditions.coupon)        : null;
-  const moneyOk    = conditions.pay    ? eval(`${totalBill}${conditions.pay}`)      : null;
-  const peopleOk   = conditions.people ? eval(`${totalPeople}${conditions.people}`) : null;
+  const evalString = deal.conditions
+    .replace('_people', totalPeople)
+    .replace('_pay', totalBill)
 
-  if (conditions.type === 'OR') {
-    return couponOk === true || moneyOk === true || peopleOk === true
-  }
-  else if (conditions.type === 'AND') {
-    return (couponOk === true || couponOk === null)
-      &&   (moneyOk  === true || moneyOk  === null)
-      &&   (peopleOk === true || peopleOk === null)
-  }
+  return coupons.length > 0
+    ? coupons.filter(coupon => eval(evalString.replace('_coupon', coupon))) > 0
+    : eval(evalString)
 }
 
 // for mapping
